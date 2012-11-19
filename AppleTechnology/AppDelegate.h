c6dbb0e75a8bfa727535b3f9bbe8bf2b3997a01b
Ahad <ali@Ahads-MacBook-Pro-2.local>
2012-11-19 23:32:15 +0330

    Initial Commit

---
+++ AppleTechnology/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AppleTechnology
+//
+//  Created by Ahad on 11/19/12.
+//  Copyright (c) 2012 Ahad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AppleTechnology/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppleTechnology
+//
+//  Created by Ahad on 11/19/12.
+//  Copyright (c) 2012 Ahad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
